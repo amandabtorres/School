dd2fc98ea83ed9e47e69fb79c2a80b9b7c5a8c80
amandabtorres <banaszeskiamanda@gmail.com>
2024-10-11 15:46:13 +0100

    UpdateImage

---
+++ School/Migrations/20241011140418_ModifyImage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace School.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ImageId",
+                table: "AspNetUsers",
+                type: "uniqueidentifier",
+                nullable: true,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
